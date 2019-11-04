2125ab17012e685609801c32f2f87302b822502a
lei <wslperfect@163.com>
2019-11-04 09:00:17 +0800

    添加注释

--- SPServer/IOCPBase.h
+++ SPServer/IOCPBase.h
@@ -22,9 +22,9 @@ private:
 	static BOOL GetCpuNumsAndPagesize();
 	static BOOL StartServer();					// 启动服务线程
 
-	static unsigned int _stdcall sunitthread(PVOID pVoid);
-	static unsigned int _stdcall toolthread(PVOID pVoid);
-	static unsigned int _stdcall workthread(PVOID pVoid);
+	static unsigned int _stdcall sunitthread(PVOID pVoid);	// 处理作为客户端时的发送
+	static unsigned int _stdcall toolthread(PVOID pVoid);	// 服务器accept
+	static unsigned int _stdcall workthread(PVOID pVoid);	// 服务器业务
 
 private:
 	HANDLE m_hUniqueInstance;	// HANDLE句柄指的是核心对象在某一个进程中的唯一索引，而不是指针。使用（INVALID_HANDLE_VALUE）初始化
@@ -32,9 +32,9 @@ private:
 	static LPFN_GETACCEPTEXSOCKADDRS	m_pfnAcceptExSockaddrs;
 	static LPFN_DISCONNECTEX	m_pfnConnectEx;
 	static HANDLE m_hIocp;
-	static PListen_Handle m_pListenHandle;
-	static PSock_Handle m_pSUnitHandle;
-	static SOCKET m_sockSend;
+	static PListen_Handle m_pListenHandle;	// 服务器监听
+	static PSock_Handle m_pSUnitHandle;		// 客户端连接
+	static SOCKET m_sockSend;				// 线程发送事件使用
 	static DWORD m_dwCpunums;
 	static DWORD m_dwPagesize;
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
@@ -43,24 +43,24 @@ private:
 private:
 	static void ClearSingleData(PSock_Handle _pSock_Handle, PSock_Buf _pBuf);
 
-	static BOOL postAcceptEx();
+	static BOOL postAcceptEx();	// 准备接受客户端连接
 	static void AcceptExSuccess(DWORD _dwTranstion, PVOID _pListen_Handle, PVOID _pBuf);
 	static void AcceptExFaile(PVOID _pListen_Handle, PVOID _pBuf);
 
-	static BOOL postZeroRecv(PSock_Handle _pSock_Handle, PSock_Buf _pBuf);
+	static BOOL postZeroRecv(PSock_Handle _pSock_Handle, PSock_Buf _pBuf);	// 检查缓存区是否有接收数据
 	static void ZeroRecvSuccess(DWORD _dwTranstion, PVOID _pSock_Handle, PVOID _pBuf);
 	static void ZeroRecvFaile(PVOID _pSock_Handle, PVOID _pBuf);
 
-	static BOOL postRecv(PSock_Handle _pSock_Handle, PSock_Buf _pBuf);
+	static BOOL postRecv(PSock_Handle _pSock_Handle, PSock_Buf _pBuf);	// 接收数据
 	static void RecvSuccess(DWORD _dwTranstion, PVOID _pSock_Handle, PVOID _pBuf);
 	static void RecvFaile(PVOID _pSock_Handle, PVOID _pBuf);
 
-	static BOOL postSend(PSock_Handle _pSock_Handle, PSock_Buf _pBuf);
+	static BOOL postSend(PSock_Handle _pSock_Handle, PSock_Buf _pBuf);	// 发送数据
 	static void SendSuccess(DWORD _dwTranstion, PVOID _pSock_Handle, PVOID _pBuf);
 	static void SendFaile(PVOID _pSock_Handle, PVOID _pBuf);
 
 private:
-	static void DoWorkProcessSuccess(DWORD _dwTranstion, PVOID _pBuf, PVOID pBuf_);
+	static void DoWorkProcessSuccess(DWORD _dwTranstion, PVOID _pBuf, PVOID pBuf_);	// 业务处理
 	static void DoWorkProcessFaile(PVOID _pBuf, PVOID pBuf_);
 /*-------------------------------------------------------------------------------------------------*/
 
@@ -68,12 +68,12 @@ private:
 private:
 	static void FD_Connect();
 	static void FD_Read();
-	static void FD_Write();
+	static void FD_Write();	// 缓冲区满后可用
 	static void FD_Close();
-	static void Fuc_SUnit(DWORD _dwIndex);
+	static void Fuc_SUnit(DWORD _dwIndex);	// 发送事件处理过程
 
-	static void Send_PostEventMessage(TCHAR* _buf, DWORD _bufsize);
-	static void Fuc_Send(DWORD _dwIndex);
+	static void Send_PostEventMessage(TCHAR* _buf, DWORD _bufsize);	// 发送事件
+	static void Fuc_Send(DWORD _dwIndex);	// 主动发送数据
 
 private:
 	static CBufferRing* m_pCBufRing;
